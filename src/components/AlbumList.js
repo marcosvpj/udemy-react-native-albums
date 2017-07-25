315d4ef3baf2260b22ed3df989645bb236f2094d
Marcos V. Pagelkopf Jr <marcos@marcosvpj.com.br>
2017-07-25 20:41:13 -0300

    Create AlbumList component

---
+++ src/components/AlbumList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Header from './Header';
+
+const AlbumList = () => {
+  return (
+    <View>
+      <Text>Albums list!!!</Text>
+    </View>
+  )
+};
+
+export default AlbumList;
+
