f0a2935f2ee6cb32b4dcf0b4318a904002293791
Marcos V. Pagelkopf Jr <marcos@marcosvpj.com.br>
2017-07-25 20:13:38 -0300

    Add new styling for the header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 const Header = () => {
-	return <Text>Header</Text>;
+	const { viewStyle, textStyle } = styles;
+
+	return (
+    <View style={viewStyle}>
+      <Text style={textStyle} >Albums!</Text>
+    </View>
+  );
+}
+
+const styles = {
+	textStyle: {
+		fontSize: 20
+	},
+	viewStyle: {
+		backgroundColor: '#f8f8f8',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+		paddingTop: 15,
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 2},
+		shadowOpacity: 0.2,
+		elevation: 2,
+		position: 'relative'
+	}
 }
 
 export default Header;
