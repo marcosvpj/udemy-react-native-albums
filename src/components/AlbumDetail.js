2c5657bf8cbe13ddc4cdbe1e0de306e926fb5b04
Marcos V. Pagelkopf Jr <marcos@marcosvpj.com.br>
2017-07-25 22:10:57 -0300

    Create CardSection component for styling

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
   return (
     <Card>
+      <CardSection>
         <Text>{props.album.title}</Text>
         <Text>{props.album.artist}</Text>
+      </CardSection>
     </Card>
   );
 }
