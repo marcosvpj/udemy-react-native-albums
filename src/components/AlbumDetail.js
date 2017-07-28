783f4aa1408a4aa64c5dd98d900c5bffe1bb276e
Marcos V. Pagelkopf Jr <marcos@marcosvpj.com.br>
2017-07-27 22:20:46 -0300

    Make button text reusable

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -38,7 +38,7 @@ const AlbumDetail = (props) => {
       </CardSection>
 
       <CardSection>
-        <Button onPress={() => Linking.openURL(url)}/>
+        <Button onPress={() => Linking.openURL(url)}>Buy Now</Button>
       </CardSection>
     </Card>
   );
